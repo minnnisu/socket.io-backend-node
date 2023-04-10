c55e0e70ae3028308bea026ef9c3d76353f07736
minnnisu <mine52100@naver.com>
2023-04-11 01:03:25 +0900

    버그 발견 후 임시 조치 완료

--- socket-server.js
+++ socket-server.js
@@ -4,6 +4,7 @@ var fs = require("fs");
 const { Transform, Writable, Readable, Duplex } = require("stream");
 
 const app = express();
+const consumerSockets = [];
 const port = 3001;
 var http = require("http").createServer(app);
 const io = require("socket.io")(http, {
@@ -15,16 +16,31 @@ const io = require("socket.io")(http, {
 });
 
 io.on("connection", (socket) => {
+  consumerSockets.push(socket);
+  console.log("  consumerSockets.length: ", consumerSockets.length);
+
   // send text message
-  socket.on("send message", (msg) => {
+  socket.on("sendMsg", (msg) => {
     console.log(`text message : (${msg.name}) ${msg.msg}`);
-    io.emit("receive message", { name: msg.name, msg: msg.msg });
+    io.emit("receiveMsg", { name: msg.name, msg: msg.msg });
   });
 
-  ss(socket).on("file", function (stream, size) {
-    const newStream = ss.createStream();
-    ss(socket).emit("receive", newStream, size);
-    stream.pipe(newStream);
+  // ss(socket).on("sendImg", function (stream, size) {
+  //   const newStream = ss.createStream();
+  //   ss(socket).emit("receiveImg", newStream, size);
+  //   stream.pipe(newStream);
+  // });
+
+  // receive the image
+  ss(socket).on("sendImg", function (incomingStream, data) {
+    console.log("on image ", data);
+
+    for (var i in consumerSockets) {
+      var consumerSocket = consumerSockets[i];
+      var outgoingStream = ss.createStream();
+      ss(consumerSocket).emit("receiveImg", outgoingStream, data);
+      incomingStream.pipe(outgoingStream);
+    }
   });
 
   socket.on("disconnect", function () {
